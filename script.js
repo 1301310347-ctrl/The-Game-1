5556fa6befb7fdb2aca09009e5f632babd2e9cbf
1301310347-ctrl <1301310347@s.tusd1.org>
2026-04-06 18:31:57 +0000

    hflbkijrenfl

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const { captureOwnerStack } = require("react");
 
 console.log("script started");
 
 function start() {
-    console.log(Math.random() * window.innerWidth);
     console.log("clicking clicking");
-    createCircle();
+    setTimeout(createCircle, 250, 1);
+    setTimeout(createCircle, 500, 2);
+    setTimeout(createCircle, 1000, 3)
+    
 }
 
-function createCircle() {
+function createCircle(num) {
     //create a div fir the circle 
     let circle = document.createElement("div");
 
 
     //set some text and style
-    circle.innerText = "1";
+    circle.innerText = num;
 
 
     
@@ -29,21 +30,35 @@ function createCircle() {
     circle.style.alignContent = "center";
     //position circle randomely using absolute
     circle.style.position = "absolute";
-    circle.style.left = getRandomX + "px";
-    circle.style.top = getRandomY + "px";
+    circle.style.left = getRandomX() + "px";
+    circle.style.top = getRandomY() + "px";
+    
     
      //add to the page 
     document.body.appendChild(circle);
+
+    //make it clickable 
+    Element.addEventListener("click", checkForRemove);
 }
 
 function getRandomX() {
-    let randX = Math.random() * window.innerWidth - 100;
+    let randX = Math.random() * (window.innerWidth - 100);
     return randX;
 }
 
 function getRandomY() {
-    let ranY = Math.random() * window.innerHeight - 100;
+    let randY = Math.random() * (window.innerHeight - 100);
     return randY;
 }
 
+//check for the right choice and remove it
+
+function checkForRemove(event) {
+    console.log(circle.innertText);
+    let circle = event.target;
+
+
+
+
+}
 
